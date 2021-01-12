61a5d194db22c4bf0e8853759a56bbfd1ab15ed4
Lycoriz6112700 <suzume.pang@gmail.com>
2021-01-12 13:58:18 +0700

    Ver 0

--- app_template.js
+++ app_template.js
@@ -1,1 +1,12 @@
-//your code here!
+//your code here
+var modifySID = 620612167;
+var row = 0;
+var col = 0;
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('#button-compute').disable = true;
+
+    document.querySelector('#button-compute').onclick = function(){
+    
+    }
+}
